b463a389568dbc8d1d3d10ba4b7b12f5ec528e28
jasonthuynh <jasonhuynh512@gmail.com>
2024-09-09 23:26:48 -0500

    added button to change styles

--- scripts/functions.js
+++ scripts/functions.js
@@ -2,3 +2,26 @@ function scrollToBottom() {
     let bottom = document.getElementById('second-section');
     bottom.scrollIntoView({behavior: "smooth"});
   }
+
+function toggleStyleSheet() {
+    let curr = document.getElementById("mainStyleSheet").getAttribute("href");
+    if (curr === "styles/style1.css") {
+      document.getElementById("mainStyleSheet").setAttribute("href", "styles/style2.css");
+      localStorage.setItem("stylesheet", "styles/style2.css");
+    }
+    else {
+      document.getElementById("mainStyleSheet").setAttribute("href", "styles/style1.css");
+      localStorage.setItem("stylesheet", "styles/style1.css");
+    }
+}
+
+window.onload = function() {
+    let savedStyle = localStorage.getItem("stylesheet");
+    let currHTML = document.getElementById("mainStyleSheet");
+    if (savedStyle) {
+      currHTML.setAttribute("href", savedStyle);
+    }
+    else {
+      currHTML.setAttribute("href", "styles/style1.css")
+    }
+}

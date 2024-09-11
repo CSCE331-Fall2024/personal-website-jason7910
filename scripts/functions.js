77e6a28ce5c8e074c6bbe2f8877c5aea2d399f9c
jasonthuynh <jasonhuynh512@gmail.com>
2024-09-11 00:59:54 -0500

    made second style for landing page

--- scripts/functions.js
+++ scripts/functions.js
@@ -5,14 +5,30 @@ function scrollToBottom() {
 
 function toggleStyleSheet() {
     let curr = document.getElementById("mainStyleSheet").getAttribute("href");
+    let sty;
     if (curr === "styles/style1.css") {
       document.getElementById("mainStyleSheet").setAttribute("href", "styles/style2.css");
       localStorage.setItem("stylesheet", "styles/style2.css");
+      sty = 1
     }
     else {
       document.getElementById("mainStyleSheet").setAttribute("href", "styles/style1.css");
       localStorage.setItem("stylesheet", "styles/style1.css");
+      sty = 0
     }
+
+    let homeIcon = document.getElementById("transition-btn");
+    let homeImage = document.getElementById("main-photo");
+    if (sty === 1) {
+      homeIcon.src = "resources/info-circle-fill.svg";
+      homeImage.src = "resources/casual-pic.jpg";
+    }
+    else {
+      homeIcon.src = "resources/info-circle-solid.svg";
+      homeImage.src = "resources/Professional_HeadShot.jpg";
+    }
+    
+    
 }
 
 window.onload = function() {

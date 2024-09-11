2cb28823609586e84b2c982d46decea684c84180
jasonthuynh <jasonhuynh512@gmail.com>
2024-09-11 18:25:56 -0500

    invert images instead of changing images

--- scripts/functions.js
+++ scripts/functions.js
@@ -4,8 +4,6 @@ function scrollToBottom() {
   }
 
 function toggleStyleSheet() {
-  // let resumeDownload = document.getElementById("download-icon");
-  // resumeDownload.src = "resources/download-white.svg";
     let curr = document.getElementById("mainStyleSheet").getAttribute("href");
     let sty;
     if (curr === "styles/style1.css") {
@@ -19,40 +17,31 @@ function toggleStyleSheet() {
       sty = 0
     }
     // sty = 1 is dark and sty = 0 is light
-    let homeIcon = document.getElementById("transition-btn");
     let homeImage = document.getElementById("main-photo");
     if (sty === 1) {
-      homeIcon.src = "resources/info-circle-fill.svg";
       homeImage.src = "resources/casual-pic.jpg";
-      resumeDownload.src = "resources/download-white.svg";
     }
     else {
-      homeIcon.src = "resources/info-circle-solid.svg";
       homeImage.src = "resources/Professional_HeadShot.jpg";
-      resumeDownload.src = "resources/download.png";
     }
 }
 
 window.onload = function() {
     let savedStyle = localStorage.getItem("stylesheet");
     let currHTML = document.getElementById("mainStyleSheet");
-    let currInfoBtn = document.getElementById("transition-btn");
     let currPortrait = document.getElementById("main-photo");
 
     if (savedStyle) {
       currHTML.setAttribute("href", savedStyle);
       if (savedStyle === "styles/style1.css") {
-        currInfoBtn.src = "resources/info-circle-solid.svg";
         currPortrait.src = "resources/Professional_HeadShot.jpg";
       }
       else {
-        currInfoBtn.src = "resources/info-circle-fill.svg";
         currPortrait.src = "resources/casual-pic.jpg";
       }
     }
     else {
       currHTML.setAttribute("href", "styles/style1.css")
-      currInfoBtn.src = "resources/info-circle-solid.svg";
       currPortrait.src = "resources/Professional_HeadShot.jpg";
     }
 }
